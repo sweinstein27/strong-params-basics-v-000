417334ffa13e1f35524672faf36d3bb2380a3fa4
Scott Weinstein <sweinstein27@gmail.com>
2018-09-06 14:14:56 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   @post.save
   redirect_to post_path(@post)
 end
- 
+
 def update
   @post = Post.find(params[:id])
   @post.update(post_params)
@@ -26,4 +26,11 @@ end
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	private
+ 
+def post_params
+  params.require(:post).permit(:title, :description)
+end
+
 end
